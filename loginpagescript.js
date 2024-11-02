d253317a493e361fde5fe0961b770bb24267fe11
pragyanshu-kashyap <162783558+pragyanshu-kashyap@users.noreply.github.com>
2024-11-02 12:48:57 +0530

    Added all files
    completed project on vs code and added here

---
+++ loginpagescript.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'user' && password === 'pass') {
+        alert('Login successful!');
+    } else {
+        alert('Invalid username or password.');
+    }
+});
